d0995099c222c311e03a675673267754eaaf6158
Silvia Perez <silviaperezjaime@gmail.com>
2021-02-28 11:27:42 +0100

    Created proyect

---
+++ adidas-subscriptionservice/src/main/java/com/adidas/subscriptionservice/AdidasSubscriptionserviceApplication.java
@@ -0,0 +1,13 @@
+package com.adidas.subscriptionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdidasSubscriptionserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdidasSubscriptionserviceApplication.class, args);
+	}
+
+}

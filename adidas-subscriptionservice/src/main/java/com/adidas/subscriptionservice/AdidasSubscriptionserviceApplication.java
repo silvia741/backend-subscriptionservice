39ddb60e8446acaae02138e48d42a716e39e7871
Silvia Perez <silviaperezjaime@gmail.com>
2021-02-28 18:27:48 +0100

    Added discovery client annotation

--- adidas-subscriptionservice/src/main/java/com/adidas/subscriptionservice/AdidasSubscriptionserviceApplication.java
+++ adidas-subscriptionservice/src/main/java/com/adidas/subscriptionservice/AdidasSubscriptionserviceApplication.java
@@ -2,8 +2,10 @@ package com.adidas.subscriptionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AdidasSubscriptionserviceApplication {
 
 	public static void main(String[] args) {

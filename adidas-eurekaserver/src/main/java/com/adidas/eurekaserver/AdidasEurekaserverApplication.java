236dcd401953658e07e3f5fc7aa07527e65fcff9
Silvia Perez <silviaperezjaime@gmail.com>
2021-02-28 18:06:37 +0100

    Created eureka server proyect

---
+++ adidas-eurekaserver/src/main/java/com/adidas/eurekaserver/AdidasEurekaserverApplication.java
@@ -0,0 +1,13 @@
+package com.adidas.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdidasEurekaserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdidasEurekaserverApplication.class, args);
+	}
+
+}
---
+++ adidas-eurekaserver/src/test/java/com/adidas/eurekaserver/AdidasEurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adidas.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdidasEurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

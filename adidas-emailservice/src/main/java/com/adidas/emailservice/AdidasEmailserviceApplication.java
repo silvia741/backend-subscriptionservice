e2c712ba2f9d4b8c7e6d6d378098803259b0dad5
Silvia Perez <silviaperezjaime@gmail.com>
2021-03-02 21:10:56 +0100

    Added emailservice app

---
+++ adidas-emailservice/src/main/java/com/adidas/emailservice/AdidasEmailserviceApplication.java
@@ -0,0 +1,13 @@
+package com.adidas.emailservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdidasEmailserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdidasEmailserviceApplication.class, args);
+	}
+
+}
---
+++ adidas-emailservice/src/test/java/com/adidas/emailservice/AdidasEmailserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adidas.emailservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdidasEmailserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
